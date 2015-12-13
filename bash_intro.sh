0fb34da15c36fdfe5041bba964d01d9cf4ecfac6
Edwin van der Helm <vdhelm@strw.leidenuniv.nl>
2015-12-13 20:07:38 +0100

    added notes and questions

--- bash_intro.sh
+++ bash_intro.sh
@@ -6,6 +6,13 @@ $ rm -r
 $ rmdir
 $ cp -r
 $ mv
+
+# Exercize: without leaving bash...
+# - create a directory for this workshop.
+# - Go to it
+# - Create a python source file with the text: "import numpy"
+
+
 $ ls -lht  # also include -a and -A
 $ ls [A-Z]*.py
 $ ls > list.txt # > vs >> and maybe <
@@ -16,6 +23,10 @@ $ du -sh * | sort -rh
 $ find -name "*.py" | xargs grep my_module
 $ find -name "*.py" | xargs wc -l | sort -rn | head
 
+# Exercize:
+# - find all uses of numpy in python source files in directories whose name ends in '_src' (or their subdirectories)
+# - find the file with the largest number of characters in any subdirectory
+
 $ for filename in *.dat
 > do
 >     cp $filename original-$filename #First just echo
@@ -24,6 +35,11 @@ $ for filename in *.dat
 # .bashrc
 alias cdc='cd /home/edwin/research/code/Wind'
 set -o vi
+
+# Exercize:
+# - create a short alias to a folder you use often in your bashrc.
+# - use this alias to go to the directory
+# - get back to your previous directory
 
 # scripting
 #!/bin/bash
